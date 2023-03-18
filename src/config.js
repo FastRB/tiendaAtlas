b8f38f5027462cbbe9ede9454e64df76864071cf
FastRB <rbucio123@live.com.mx>
2023-03-18 16:44:47 -0600

    quite el port

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ config();
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/tienda"
 
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT || 3000;
